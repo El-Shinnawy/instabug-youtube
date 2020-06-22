9dbb4831057c040564aa460ec939cd261dcd1c7e
shennawy <abdelrahman.elshinnawy@gmail.com>
2020-06-22 20:52:39 +0200

    [Development] {Security} Remove API Key from github repo

--- src/configurations.ts
+++ src/configurations.ts
@@ -1,6 +1,6 @@
 export const Configurations = {
   baseUrl: "https://www.googleapis.com/youtube/v3/",
-  apiKey: "AIzaSyAl6CLHYepOzoyIHGcoKvynW22ZPlv445k",
+  apiKey: "",
   thumbnailBaseUrl: "https://img.youtube.com/vi/VIDEO_ID/0.jpg",
   searchResultsCount: 25
 };
